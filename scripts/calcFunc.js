395832fc53b35b8bfdaf81513b6c9c87f971cb36
John Williams <johnnjit7@gmail.com>
2017-12-31 13:02:58 -0500

    Added custom classes to buttons and click events to buttons

--- scripts/calcFunc.js
+++ scripts/calcFunc.js
@@ -1,19 +1,48 @@
 $(document).ready(function() {
 
-  var zero = $('#zero');
-  var one = $('#one');
-  var two = $('#two');
-  var three = $('#three');
-  var four = $('#four');
-  var five = $('#five');
-  var six = $('#six');
-  var seven = $('#seven');
-  var eight = $('#eight');
-  var nine = $('#nine');
+  var accumulator = "";
+  var currentEntry = "";
+  var entireOperation = "";
 
-  $('#zero, #one, #two, #three, #four, #five, #six, #seven, #eight, #nine').on('click', function() {
-    console.log("Button pressed: " + this.id);
+  $('.digitBtn').on('click', function() {
+    let buttonID = this.id;
+    let buttonIDVal = document.getElementById(buttonID);
+    let buttonVal = $( buttonIDVal ).text();
+    console.log("Button pressed: " + buttonVal);
+  /*  if(buttonIDVal === "decimal") {
+      currentEntry += '.';
+    }
+    else {*/
+      currentEntry += buttonVal;
+    //}
+
+    console.log(currentEntry);
   });
 
+  $('.arithmeticBtn').on('click', function() {
+    let buttonId = this.id;
+    let buttonIdVal = document.getElementById(buttonId);
+    let buttonValue = $( buttonIdVal ).text();
+    console.log("Button pressed: " + buttonValue);
+    if(accumulator !== "") {
+      let tempAcc = Number(accumulator);
+      let tempCurr = Number(currentEntry);
+      let tempOperation = accumulator + currentEntry + buttonValue ;
+      console.log(tempOperation);
+    }
+    else {
+      accumulator = currentEntry + buttonValue;
+      currentEntry = "";
+      console.log(accumulator);
+    }
+
+  });
+
+  $('.funcBtn').on('click', function() {
+    let buttonId = this.id;
+    let buttonIdValue = document.getElementById(buttonId);
+    let buttonFunc = $( buttonIdValue ).text();
+    console.log("Button pressed: " + buttonFunc);
+  });
 
 });

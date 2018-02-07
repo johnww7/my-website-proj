2500483e24121ac0dc425085da7eb29ad7d28ad0
John Williams <johnnjit7@gmail.com>
2018-02-07 15:04:40 -0500

    Changed id for board spaces in tictacgame file and added click listener to modal buttons and class boardSpace in tictacgame file

--- scripts/tictacgame.js
+++ scripts/tictacgame.js
@@ -1,5 +1,35 @@
 $(document).ready(function() {
   $('#gameIntro').modal('show');
 
+  let playerChoice = '';
+  let computerChoice = '';
+  let originalBoard = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
+
+  $('#chooseX, #chooseY').on('click', function() {
+    let choice = $(this).attr('id');
+
+    if(choice == 'chooseX') {
+      playerChoice = 'X';
+      computerChoice ='O';
+    }
+    else {
+      playerChoice = 'O';
+      computerChoice = 'X';
+
+    }
+
+    $('#gameIntro').modal('hide');
+  });
+
+  $('.boardSpace').on('click', function() {
+    let markedSpace = $(this).attr('id');
+
+    
+
+  });
+
+
+
+
 
 });

62912b5cc3a6e9dc42aef4afa793d593ba5d02e4
John Williams <johnnjit7@gmail.com>
2018-03-10 16:19:35 -0500

    Made circular boards using transform and border radius for possible board

--- scripts/simonGame.js
+++ scripts/simonGame.js
@@ -1,5 +1,28 @@
 $(document).ready(function() {
 
-  
+  /*
+  <div id="topPanel" class="row outside-border justify-content-center">
+    <div class="col-sm-11 upper-circle">
+    </div>
+  </div>
+
+  <div id="middlePanel" class="row outside-border">
+    <div class="col-sm">
+
+    </div>
+    <div class="col-sm">
+
+    </div>
+    <div class="col-sm">
+    </div>
+  </div>
+
+  <div id="bottomPanel" class="row outside-border">
+    <div class="col-sm-12">
+
+    </div>
+  </div>
+
+  */
 
 });

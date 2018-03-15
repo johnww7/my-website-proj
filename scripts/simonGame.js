f19316952620420c4afa576c1a98c0ec1533bcb1
John Williams <johnnjit7@gmail.com>
2018-03-15 14:41:20 -0400

    Colored simon board on canvas and changed panel sizes for border-radius simon board

--- scripts/simonGame.js
+++ scripts/simonGame.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   drawSimonBoard();
+  setTimeout(drawHighlight, 5000);
 
   function drawSimonBoard() {
     let canvas = document.getElementById('simonCanvas');
@@ -31,8 +32,8 @@ $(document).ready(function() {
 
     //draw panel 1 slice
     board.beginPath();
-    board.lineWidth = "2";
-    board.strokeStyle = "red";
+    //board.lineWidth = "2";
+
     board.moveTo(355, 25);
     board.lineTo(355, 345);
     board.moveTo(675, 345);
@@ -40,25 +41,33 @@ $(document).ready(function() {
     let panelOneStart = (Math.PI/180) * 270;
     let panelOneEnd = (Math.PI/180) *360;
     board.arc(355, 345, 320, panelOneStart, panelOneEnd);
+    board.closePath();
+    board.lineWidth = "2"
+    board.strokeStyle = "black";
+    board.fillStyle = "red";
+    board.fill();
     board.stroke();
 
     //draw panel 2 slice
     board.beginPath();
-    board.lineWidth = "2"
-    board.strokeStyle = "red";
-    board.moveTo(355, 355);
-    board.lineTo(355, 675);
-    board.moveTo(675, 355);
+    board.moveTo(355, 675);
     board.lineTo(355, 355);
-    let panelTwoStart = (Math.PI/180) * 0;
+    board.lineTo(675, 355);
+
+    let panelTwoStart = (Math.PI/180) * 360;
     let panelTwoEnd = (Math.PI/180) * 90;
     board.arc(355, 355, 320, panelTwoStart, panelTwoEnd);
+    board.closePath();
+    board.lineWidth = "2"
+    board.strokeStyle = "black";
+    board.fillStyle = "blue";
+    board.fill();
     board.stroke();
 
     //draw panel 3 slice
     board.beginPath();
-    board.lineWidth = "2";
-    board.strokeStyle = "red";
+    //board.lineWidth = "2";
+
     board.moveTo(345,675);
     board.lineTo(345, 355);
     board.moveTo(25, 355);
@@ -66,19 +75,106 @@ $(document).ready(function() {
     let panelThreeStart = (Math.PI/180) * 90;
     let panelThreeEnd = (Math.PI/180) *180;
     board.arc(345, 355, 320, panelThreeStart, panelThreeEnd);
-    board.stroke();
+    board.closePath();
+    board.fillStyle = "green";
+    board.fill();
 
     //draw panel 4 slice
     board.beginPath();
-    board.lineWidth = "2";
-    board.strokeStyle = "red";
+  //  board.lineWidth = "2";
+
     board.moveTo(345, 25);
     board.lineTo(345, 345);
-    board.moveTo(25, 345);
-    board.lineTo(345,345);
+    board.lineTo(25, 345);
+
     let panelFourStart = (Math.PI/180) * 180;
     let panelFourEnd = (Math.PI/180) * 270;
     board.arc(345, 345, 320, panelFourStart, panelFourEnd);
+    board.closePath();
+    board.lineWidth = "2";
+    board.strokeStyle = 'black';
+    board.fillStyle = "yellow";
+    board.fill();
+    board.stroke();
+  }
+
+  function drawHighlight() {
+    let canvas = document.getElementById('simonCanvas');
+    let board = canvas.getContext('2d');
+
+    let circleX = canvas.width/2;
+    let circleY = canvas.height/2;
+    let circleRadius = 325;
+    let startAngle = 0;
+    let endAngle = 2*Math.PI;
+
+    //Draws game board
+
+    //draw panel 1 slice
+    board.beginPath();
+    //board.lineWidth = "2";
+
+    board.moveTo(355, 25);
+    board.lineTo(355, 345);
+    board.moveTo(675, 345);
+    board.lineTo(355, 345);
+    let panelOneStart = (Math.PI/180) * 270;
+    let panelOneEnd = (Math.PI/180) *360;
+    board.arc(355, 345, 320, panelOneStart, panelOneEnd);
+    board.closePath();
+    board.lineWidth = "2"
+    board.strokeStyle = "black";
+    board.fillStyle = "#ff7f6a";
+    board.fill();
+    board.stroke();
+
+    //draw panel 2 slice
+    board.beginPath();
+    board.moveTo(355, 675);
+    board.lineTo(355, 355);
+    board.lineTo(675, 355);
+
+    let panelTwoStart = (Math.PI/180) * 360;
+    let panelTwoEnd = (Math.PI/180) * 90;
+    board.arc(355, 355, 320, panelTwoStart, panelTwoEnd);
+    board.closePath();
+    board.lineWidth = "2"
+    board.strokeStyle = "black";
+    board.fillStyle = "#0077ff";
+    board.fill();
+    board.stroke();
+
+    //draw panel 3 slice
+    board.beginPath();
+    //board.lineWidth = "2";
+
+    board.moveTo(345,675);
+    board.lineTo(345, 355);
+    board.moveTo(25, 355);
+    board.lineTo(345, 355);
+    let panelThreeStart = (Math.PI/180) * 90;
+    let panelThreeEnd = (Math.PI/180) *180;
+    board.arc(345, 355, 320, panelThreeStart, panelThreeEnd);
+    board.closePath();
+    board.fillStyle = "#2fff00";
+    board.fill();
+
+    //draw panel 4 slice
+    board.beginPath();
+  //  board.lineWidth = "2";
+
+    board.moveTo(345, 25);
+    board.lineTo(345, 345);
+    board.lineTo(25, 345);
+
+    let panelFourStart = (Math.PI/180) * 180;
+    let panelFourEnd = (Math.PI/180) * 270;
+    board.arc(345, 345, 320, panelFourStart, panelFourEnd);
+    board.closePath();
+    board.lineWidth = "2";
+    board.strokeStyle = 'black';
+    board.fillStyle = "#ffff5f";
+    board.fill();
     board.stroke();
   }
 

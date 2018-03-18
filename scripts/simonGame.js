fe04a5d4e9c8894600125cff1eb9f74903710797
John Williams <johnnjit7@gmail.com>
2018-03-18 16:53:17 -0400

    Added functionality to panel shapes when pressed down and mouse up

--- scripts/simonGame.js
+++ scripts/simonGame.js
@@ -2,15 +2,15 @@ $(document).ready(function() {
 
   let canvas = document.getElementById('simonCanvas');
   let board = canvas.getContext('2d');
-
+//#ff0000
   const SHAPE_ARRAY = [
-    {'id': 1, 'color': ["rgba(250, 5, 5, 0.5)", "#ff0000"], 'start': [355,25],'lines': [355, 345, 675, 345],
+    {'id': 1, 'color': ["#e97c7c", "#ff0000"], 'start': [355,25],'lines': [355, 345, 675, 345],
     'arc': [355, 345, 320, ((Math.PI/180) * 270), ((Math.PI/180) *360)]},
     {'id': 2, 'color': ["#4d44a9", "#0024ff"], 'start': [355,675],'lines': [355, 355, 675, 355],
     'arc': [355, 355, 320, ((Math.PI/180) * 360), ((Math.PI/180) *90)]},
     {'id': 3, 'color': ["#3b8a3b", "#00ff00"], 'start': [345,675],'lines': [345, 355, 25, 355],
     'arc': [345, 355, 320, ((Math.PI/180) * 90), ((Math.PI/180) *180)]},
-    {'id': 4, 'color': ["rgba(255, 255, 0, 0.5)", "#ffff00"], 'start': [345,25],'lines': [345, 345, 25, 345],
+    {'id': 4, 'color': ["#ffffb8", "#ffff00"], 'start': [345,25],'lines': [345, 345, 25, 345],
     'arc': [345, 345, 320, ((Math.PI/180) * 180), ((Math.PI/180) *270)]}
   ];
 
@@ -53,21 +53,82 @@ $(document).ready(function() {
     }
   }
 
-  document.getElementById('simonCanvas').addEventListener("click", function(event) {
+  function drawShape(shapeObj, hightLight){
+    let keys = Object.keys(shapeObj);
+    let shapeStart = keys[2];
+    let shapeLines = keys[3];
+    let shapeArc = keys[4];
+    let id = keys[0];
+    let startPoint = shapeObj[shapeStart];
+    let lines = shapeObj[shapeLines];
+    let arc = shapeObj[shapeArc];
+    let regionID = shapeObj[id];
+    let color = '';
+
+    if(hightLight === true) {
+      color = shapeObj['color'][1];
+    }
+    else {
+      color = shapeObj['color'][0];
+    }
+    //board.clearRect(arc[0], arc[1], arc[0]+320, arc[1]+320);
+    //board.clearRect(0, 0, canvas.width, canvas.height);
+    console.log(color);
+
+    board.beginPath();
+    board.moveTo(startPoint[0], startPoint[1]);
+    board.lineTo(lines[0], lines[1]);
+    board.lineTo(lines[2], lines[3]);
+    board.arc(arc[0], arc[1], arc[2], arc[3], arc[4]);
+
+    board.fillStyle = color;
+    board.fill();
+    board.addHitRegion({id: regionID});
+
+  }
+
+  document.getElementById('simonCanvas').addEventListener("mousedown", function(event) {
     if (event.region) {
 
       switch(event.region) {
         case '1':
-          alert('shape 1');
+          console.log('shape 1');
+          drawShape(SHAPE_ARRAY[0], true);
           break;
         case '2':
-          alert('shape 2');
+          //alert('shape 2');
+          drawShape(SHAPE_ARRAY[1], true);
           break;
         case '3':
-          alert('shape 3');
+          //alert('shape 3');
+          drawShape(SHAPE_ARRAY[2], true);
           break;
         case '4':
-          alert('shape 4');
+          //alert('shape 4');
+          drawShape(SHAPE_ARRAY[3],true);
+          break;
+      }
+    }
+  });
+
+  document.getElementById('simonCanvas').addEventListener("mouseup", function(event) {
+    if(event.region) {
+      switch(event.region) {
+        case '1':
+          //alert('shape 1');
+          drawShape(SHAPE_ARRAY[0], false);
+          break;
+        case '2':
+          //alert('shape 2');
+          drawShape(SHAPE_ARRAY[1], false);
+          break;
+        case '3':
+          //alert('shape 3');
+          drawShape(SHAPE_ARRAY[2],false);
+          break;
+        case '4':
+          //alert('shape 4');
+          drawShape(SHAPE_ARRAY[3], false);
           break;
       }
     }

3968e63d7c01864890e675f1bebefa5be3cba8fe
John Williams <johnnjit7@gmail.com>
2018-04-22 15:39:35 -0400

    Added more files and continuing react tutorial

---
+++ react-tutorial/src/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Layout extends React.Component {
+  constructor() {
+    super();
+    this.state ={title: "Welcome"};
+    this.changeTitle = this.changeTitle.bind(this);
+  }
+
+  changeTitle(title) {
+    this.setState({title});
+  }
+
+  render() {
+    return (
+      <div>
+        <Header changeTitle = {this.changeTitle} title ={this.state.title}/>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+class Header extends React.Component {
+  handleChange(e) {
+    const title = e.target.value;
+    this.props.changeTitle(title);
+  }
+
+  render() {
+    return (
+      <div>
+        <Title title={this.props.title} />
+        <input value={this.props.title} onChange={this.handleChange.bind(this)}/>
+      </div>
+    );
+  }
+}
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>footer</footer>
+    );
+  }
+}
+
+class Title extends React.Component {
+  render() {
+    return (
+      <h1>{this.props.title}</h1>
+    );
+  }
+}
+
+const app = document.getElementById('app');
+ReactDOM.render(<Layout/>, app);

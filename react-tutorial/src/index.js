f6614d9deb2babae75a8870392bcc2d64576dbff
John Williams <johnnjit7@gmail.com>
2018-04-23 15:29:24 -0400

    Learning react routes

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -1,58 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-class Layout extends React.Component {
-  constructor() {
-    super();
-    this.state ={title: "Welcome"};
-    this.changeTitle = this.changeTitle.bind(this);
-  }
-
-  changeTitle(title) {
-    this.setState({title});
-  }
-
-  render() {
-    return (
-      <div>
-        <Header changeTitle = {this.changeTitle} title ={this.state.title}/>
-        <Footer />
-      </div>
-    );
-  }
-}
-
-class Header extends React.Component {
-  handleChange(e) {
-    const title = e.target.value;
-    this.props.changeTitle(title);
-  }
-
-  render() {
-    return (
-      <div>
-        <Title title={this.props.title} />
-        <input value={this.props.title} onChange={this.handleChange.bind(this)}/>
-      </div>
-    );
-  }
-}
-
-class Footer extends React.Component {
-  render() {
-    return (
-      <footer>footer</footer>
-    );
-  }
-}
-
-class Title extends React.Component {
-  render() {
-    return (
-      <h1>{this.props.title}</h1>
-    );
-  }
-}
+import {Layout} from "./pages/Layout";
+import {Featured} from "./pages/Featured";
+import {Archives} from "./pages/Archives";
+import {Settings} from "./pages/Settings";
 
 const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
+ReactDOM.render(
+  <Router>
+    <Route path="/" component={Layout}>
+
+    </Route>
+  </Router>,
+  app);

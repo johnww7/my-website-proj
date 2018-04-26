69da1159e86da7786e6338bfaec0eba465e20d0b
John Williams <johnnjit7@gmail.com>
2018-04-26 16:47:55 -0400

    added query string package using npm and fixed the showing query from url error

--- react-tutorial/src/pages/Archives.js
+++ react-tutorial/src/pages/Archives.js
@@ -1,10 +1,12 @@
 import React from "react";
+import queryString from 'query-string';
 
 export class Archives extends React.Component {
   render() {
     //const { query } = this.props.location;
     const { match } = this.props;
-    const {date, filter} = this.props.location.search;
+    const {date, filter} = queryString.parse(this.props.location.search);
+    console.log(queryString.parse(this.props.location.search));
     return (
       <div>
         <h3>Archives ({match.params.article})</h3>

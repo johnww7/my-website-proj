e24fc8e2740c7e862ef1f252ad6b2dd98fa7d6cd
John Williams <johnnjit7@gmail.com>
2018-05-05 13:26:40 -0400

    Got JSX element display for projectworking

--- markdown-previewer/src/index.js
+++ markdown-previewer/src/index.js
@@ -3,19 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class MarkdownContainer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '### Another deeper heading# Marked in the browserRendered by **marked**.',
+    };
+  }
 
   render() {
+    return(
+      <div className="row">
+        <div className="col-6">
+          <InputText />
+        </div>
+        <div className ="col-6">
+          <OutputText value={this.state.text} />
+        </div>
 
+      </div>
+    );
   }
 }
 
 class InputText extends React.Component {
 
   render() {
-
+// value={this.props.value} onChange={this.handleChange}
     return (
       <div>
-        <textarea value={this.props.value} onChange={this.handleChange} />
+        <textarea rows='25' cols='40'/>
       </div>
     );
   }
@@ -24,7 +40,11 @@ class InputText extends React.Component {
 class OutputText extends React.Component {
 
   render() {
-
+    return (
+      <div>
+        <pre className='output-display'>{this.props.value}</pre>
+      </div>
+    );
   }
 }
 

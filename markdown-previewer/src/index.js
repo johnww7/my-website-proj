cab167c5da65b3e6d267046516b024b9b9d36422
John Williams <johnnjit7@gmail.com>
2018-05-07 14:35:33 -0400

    Finished Markdown project

--- markdown-previewer/src/index.js
+++ markdown-previewer/src/index.js
@@ -11,59 +11,48 @@ class MarkdownContainer extends React.Component {
     };
 
     this.handleChange = this.handleChange.bind(this);
-    this.returnMarkUp = this.returnMarkUp.bind(this);
   }
 
   handleChange(event) {
     let markedText = marked(event.target.value);
-    //let docText = new DOMParser().parseFromString(markedText, 'text/html');
-    console.log(typeof markedText);
-    //let tokens = marked.lexer(event.target.value);
     this.setState({
       text: markedText,
     });
   }
 
-  returnMarkUp() {
-    return {_html: this.state.text};
-  }
-
   render() {
     return(
       <div className="row">
-        <div className="col-6">
-          <InputText onChange={this.handleChange}/>
+        <div className="col-sm-6 col-lg-6 p-2">
+          <InputTextBox onChange={this.handleChange}/>
         </div>
-        <div className ="col-6 output-display">
-          <pre dangerouslySetInnerHTML={{_html: this.state.text}} />
+        <div className ="col-sm-10 col-lg-6 p-2">
+          <OutputTextBox textValue={this.state.text}/>
         </div>
-
       </div>
     );
   }
 }
 
-class InputText extends React.Component {
+class InputTextBox extends React.Component {
 
-  render() {
-
+  render(){
     return (
-      <div>
-        <textarea rows='25' cols='40' value={this.props.value} onChange={this.props.onChange}/>
+      <div className='box-margins'>
+        <textarea className=''
+          rows='20' cols='40' value={this.props.value} onChange={this.props.onChange}/>
       </div>
     );
   }
 }
 
-class OutputText extends React.Component {
-  //
+class OutputTextBox extends React.Component {
+
   render() {
-    let outputValue = this.props.dangerouslySetInnerHTML._html;
-    //<pre className='output-display'>{outputValue}</pre>
+
     return (
-      <div className='output-display' dangerouslySetInnerHTML={{_html: outputValue}} />
-
-
+      <div className='output-display w-100 box-margins'
+        dangerouslySetInnerHTML={{__html: (this.props.textValue || '')}}/>
     );
   }
 }

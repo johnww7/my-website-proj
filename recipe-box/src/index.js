623ca8a99816fb925c3ad6c6130166565ddc9352
John Williams <johnnjit7@gmail.com>
2018-05-15 12:58:46 -0400

    Created new react project recipe box and added component classes for UI

---
+++ recipe-box/src/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+class RecipeBox extends React.Component {
+
+  render() {
+
+  }
+}
+
+class RecipeIndexList extends React.Component {
+
+  render() {
+
+  }
+}
+
+class AddRecipeArea extends React.Component {
+
+  render() {
+
+  }
+}
+
+class RecipeListItem extends React.Component {
+
+  render() {
+
+  }
+}
+
+class RecipeDetailList extends React.Component {
+
+  render() {
+
+  }
+}
+
+class AddRecipeForm extends React.Component {
+
+  render() {
+
+  }
+}
+
+class EditRecipeForm extends React.Component {
+
+  render() {
+    
+  }
+}

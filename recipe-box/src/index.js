180656ba7e4bcd06fd40f047ceb8e1ec304791f6
John Williams <johnnjit7@gmail.com>
2018-05-16 14:08:48 -0400

    Creating a static template based on react components for recipe-box

--- recipe-box/src/index.js
+++ recipe-box/src/index.js
@@ -7,13 +7,32 @@ class RecipeBox extends React.Component {
 
   render() {
 
+    return (
+      <div className="container">
+        <div className='row'>
+          <RecipeIndexList />
+        </div>
+        <div className='row'>
+          <div className='col-lg-10'>
+
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
 class RecipeIndexList extends React.Component {
 
   render() {
-
+    return(
+      <div className='col-lg-10'>
+        <ul>
+          <RecipeListItem />
+          <RecipeListItem />
+        </ul>
+      </div>
+    );
   }
 }
 
@@ -21,6 +40,9 @@ class AddRecipeArea extends React.Component {
 
   render() {
 
+    return(
+      "hi"
+    );
   }
 }
 
@@ -28,26 +50,63 @@ class RecipeListItem extends React.Component {
 
   render() {
 
+    return(
+      <li>
+        <button className='collapsible-accordion'>Recipe 1</button>
+        <div className="recipe-content">
+          <div className="recipe-detail-display">
+            <h5>Ingredients</h5>
+            <RecipeDetailList />
+
+            <div className='recipe-detail-btn-display'>
+              <button>Edit</button>
+              <button>Delete</button>
+            </div>
+          </div>
+        </div>
+      </li>
+    );
   }
 }
 
 class RecipeDetailList extends React.Component {
 
   render() {
+    const itemArray = ['item 1', 'item 2', 'item 3'];
+    const listItems = itemArray.map((value, index) =>
+      <li id={index} className="list-group-tem">{value}</li>
+    );
 
+    return(
+
+        <ul className="list-group">
+          {listItems}
+        </ul>
+
+    );
   }
 }
 
 class AddRecipeForm extends React.Component {
 
   render() {
-
+    return(
+      "hi"
+    );
   }
 }
 
 class EditRecipeForm extends React.Component {
 
   render() {
-    
+    return(
+      "hi"
+    );
   }
 }
+
+const root = document.getElementById('root');
+ReactDOM.render(
+  <RecipeBox />,
+  root
+);

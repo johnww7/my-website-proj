986bc621914f1214899e2e1a33da00bb4b8b9350
John Williams <johnnjit7@gmail.com>
2018-07-27 19:10:48 -0400

    Started new react project drum machine

---
+++ drum-machine/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+class DrumMachineContainer extends React.Component() {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    return(
+      <div id="drum-machine" class="col-sm">
+        
+      </div>
+    );
+  }
+}

b4a8ba089472ed022b4409e8c1228be6ff56a2a2
John Williams <johnnjit7@gmail.com>
2018-07-28 19:24:08 -0400

    Starting to work on layout for drummachine

--- drum-machine/src/index.js
+++ drum-machine/src/index.js
@@ -3,17 +3,82 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 
-class DrumMachineContainer extends React.Component() {
+class DrumMachineContainer extends React.Component {
   constructor(props) {
-    super(props);
+    super();
   }
 
   render() {
 
     return(
-      <div id="drum-machine" class="col-sm">
-        
+      <div id="drum-machine" className="col-sm-8">
+        <div className="row">
+          <div id="display" className="col-sm">
+
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-4 button-style">
+            <a href="#" className="drum-pad" id="Q1">
+              <span>Q</span>
+              <audio id="Q" className="clip"></audio>
+            </a>
+          </div>
+          <div className="col-4 button-style">
+            <a href="#" className="drum-pad" id="W2">
+              <span>W</span>
+              <audio id="W" className="clip"></audio>
+            </a>
+          </div>
+          <div className="col-4 button-style">
+            <a href="#" className="drum-pad" id="E3">
+              <span>E</span>
+              <audio id="E" className="clip"></audio>
+            </a>
+          </div>
+          <div className="col-4 button-style">
+            <a href="#" className="drum-pad" id="A4">
+              <span>A</span>
+              <audio id="A" className="clip"></audio>
+            </a>
+          </div>
+          <div className="col-4 button-style">
+            <a href="#" className="drum-pad" id="S5">
+              <span>S</span>
+              <audio id="S" className="clip"></audio>
+            </a>
+          </div>
+          <div className="col-4 button-style">
+            <a href="#" className="drum-pad" id="D6">
+              <span>D</span>
+              <audio id="D" className="clip"></audio>
+            </a>
+          </div>
+          <div className="col-4 button-style">
+            <a href="#" className="drum-pad" id="Z7">
+              <span>Z</span>
+              <audio id="Z" className="clip"></audio>
+            </a>
+          </div>
+          <div className="col-4 button-style">
+            <a href="#" className="drum-pad" id="X8">
+              <span>X</span>
+              <audio id="X" className="clip"></audio>
+            </a>
+          </div>
+          <div className="col-4 button-style">
+            <a href="#" className="drum-pad" id="C9">
+              <span>C</span>
+              <audio id="C" className="clip"></audio>
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+ReactDOM.render(
+  <DrumMachineContainer />,
+  document.getElementById('root')
+);

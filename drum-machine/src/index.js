fe415d5bbf31e9457f208b18181ab333b7bf46ad
John Williams <johnnjit7@gmail.com>
2018-08-14 19:00:55 -0400

    still working on refs

--- drum-machine/src/index.js
+++ drum-machine/src/index.js
@@ -37,7 +37,7 @@ class DrumMachineContainer extends React.Component {
     this.handleClick = this.handleClick.bind(this);
     this.handleKeyDown = this.handleKeyDown.bind(this);
 
-    //this.audioElement = React.createRef();
+    this.audioElement = React.createRef();
   }
 
 
@@ -50,7 +50,7 @@ class DrumMachineContainer extends React.Component {
     });
     //document.getElementById(elem.toString()).play();
     //console.log(this.refs[`drumPadElement${elem.btnText}`]);
-    console.log(this.audioElement.current);
+    console.log(this.audioElement.value);
     console.log(this.state.audioClip);
   }
 
@@ -62,7 +62,7 @@ class DrumMachineContainer extends React.Component {
     const drumElements = DRUM_PAD.map((elem) =>
       <DrumPadElement key={elem.id} padId={elem.id} text={elem.btnText} src={elem.src}
       onClick={this.handleClick.bind(this, elem.btnText, elem.id)} play={this.state.audioStart}
-      onKeyDown={this.handleKeyDown.bind(this, elem.btnText)} audioRef={aud => this.audioElement = aud}/>
+      onKeyDown={this.handleKeyDown.bind(this, elem.btnText)} ref={this.audioElement}/>
     );
 
     return(

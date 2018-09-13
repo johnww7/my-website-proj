63c1a42501d8af8e92ca1037268ec795e33cfd1e
John Williams <johnnjit7@gmail.com>
2018-09-13 18:30:27 -0400

    Worked on bugs for RoomList and finsihed it up

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -14,18 +14,21 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
+      roomId: null,
       messages: [],
       joinableRooms: [],
       joinedRooms: []
     };
 
     this.sendMessage = this.sendMessage.bind(this);
+    this.subscribeToRoom = this.subscribeToRoom.bind(this);
+    this.getRooms = this.getRooms.bind(this);
   }
 
   componentDidMount() {
     const chatManager = new ChatManager({
       instanceLocator,
-      userId: 'johnww7',
+      userId: 'niteman',
       tokenProvider: new TokenProvider({
         url: tokenUrl
       })
@@ -33,38 +36,50 @@ class App extends React.Component {
 
     chatManager.connect().then(currentUser => {
       this.currentUser = currentUser
-
-      this.currentUser.getJoinableRooms().then(
-        joinableRooms => {
-            this.setState({
-              joinableRooms,
-              joinedRooms: this.currentUser.rooms,
-            });
-        }).catch(
-          err => console.log('error on joinableRooms: ', err)
-        );
-
-      this.currentUser.subscribeToRoom({
-        roomId: 15873994,
-        hooks: {
-          onNewMessage: message => {
-            console.log('message.text', message.text);
-            this.setState({
-              messages: [...this.state.messages, message]
-            });
-          }
-        }
-      })
+      this.getRooms();
     }).catch(
       err => console.log('error on connecting: ', err)
     );
 
   }
 
+  getRooms() {
+    this.currentUser.getJoinableRooms().then(
+      joinableRooms => {
+          this.setState({
+            joinableRooms,
+            joinedRooms: this.currentUser.rooms,
+          });
+    }).catch(
+        err => console.log('error on joinableRooms: ', err)
+    );
+  }
+
+  subscribeToRoom(roomId) {
+    this.setState({ messages: [] });
+    this.currentUser.subscribeToRoom({
+      roomId: roomId,
+      hooks: {
+        onNewMessage: message => {
+          console.log('message.text', message.text);
+          this.setState({
+            messages: [...this.state.messages, message]
+          });
+        }
+      }
+    }).then( room => {
+      this.setState({
+        roomId: room.id
+      });
+        this.getRooms();
+    }).catch(err => console.log('error on subscribing to room: ', err));
+
+  }
+
   sendMessage(text) {
     this.currentUser.sendMessage({
       text,
-      roomId: 15873994
+      roomId: this.state.roomId
     });
   }
 
@@ -72,7 +87,10 @@ class App extends React.Component {
     //console.log('this.state.messages: ', this.state.messages);
         return (
             <div className="app">
-                <RoomList rooms={[...this.state.joinableRooms, ...this.state.joinedRooms]}/>
+                <RoomList
+                  roomId={this.state.roomId} 
+                  subscribeToRoom={this.subscribeToRoom}
+                  rooms={[...this.state.joinableRooms, ...this.state.joinedRooms]}/>
                 <MessageList messages={this.state.messages}/>
                 <SendMessageForm sendMessage={this.sendMessage}/>
                 <NewRoomForm />
--- chat-app/src/components/RoomList.js
+++ chat-app/src/components/RoomList.js
@@ -2,14 +2,19 @@ import React from 'react';
 
 class RoomList extends React.Component {
     render () {
-      console.log(this.props.rooms);
+      const orderedRooms = [...this.props.rooms].sort((a,b) => a.id - b.id);
+      //console.log(this.props.rooms);
         return (
             <div className="rooms-list">
               <ul>
-              {this.props.rooms.map(room => {
+              {orderedRooms.map(room => {
+                const active = this.props.roomId === room.id ? "active" : "";
                 return(
-                  <li key={room.id} className="room">
-                    <a href="#"> # {room.name} </a>
+                  <li key={room.id} className={"room " + active}>
+                    <a onClick={() => this.props.subscribeToRoom(room.id) }
+                      href="#">
+                      # {room.name}
+                    </a>
                   </li>
                 );
               })}
